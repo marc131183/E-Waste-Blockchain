c8937dfb264d81fdf9f3e8a0ea3aaa71b01b695f
marc131183 <marc.groeling@gmail.com>
2023-03-22 14:42:48 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="e-waste-blockchain",
+    version="0.1",
+    install_requires=[
+        "protobuf==3.20.1",
+        "abci==0.8.3",
+        "sqlitedict==2.1.0",
+        "ecdsa==0.18.0",
+    ],
+)
